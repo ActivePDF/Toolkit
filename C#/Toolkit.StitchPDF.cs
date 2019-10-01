ebf704b5dac17243bd34838f87f73f83a6272b98
Bruce Tompkinson <bruce.tompkinson@activepdf.com>
2019-10-01 10:30:22 -0700

    Added StitchPDF Example

---
+++ C#/Toolkit.StitchPDF.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolkitExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string inputFile = $"{strPath}Toolkit.Input.pdf";
+
+            // Instantiate Object
+            using (APToolkitNET.Toolkit toolkit = new APToolkitNET.Toolkit())
+            {
+                // Retrieves the number of pages for the specified PDF file.
+                int numPages = toolkit.NumPages(inputFile);
+                if (numPages == 0)
+                {
+                    WriteResult($"Error getting input file page count: {numPages.ToString()}", toolkit);
+                    return;
+                }
+
+                // The number of pages from the input PDF to place per row.
+                float pagesPerRow = Convert.ToSingle(Math.Ceiling(Math.Sqrt((double)numPages)));
+
+                // Close the input file before creating a new document.
+                toolkit.CloseInputFile();
+
+                // Create the new PDF file
+                int result = toolkit.OpenOutputFile($"{strPath}Toolkit.StitchPDF.pdf");
+                if (result != 0)
+                {
+                    WriteResult($"Error opening output file: {result.ToString()}", toolkit);
+                    return;
+                }
+
+                // Using the default PDF width and height of 612/792
+                float pageWidth = 612.0f, pageHeight = 792.0f;
+
+                // The width and height of each page from the original PDF
+                // added to the output file.
+                float width = pageWidth / pagesPerRow, height = pageHeight / pagesPerRow;
+
+                // The rows of images from the original PDF added to the new
+                // document.
+                int numRows = Convert.ToInt32(Math.Ceiling(pageHeight / height));
+
+                for (int i = 1; i < numRows; ++i)
+                {
+                    for (int j = 0; j < pagesPerRow; ++j)
+                    {
+                        // Add the page from the original PDF to the output.
+                        toolkit.StitchPDF(
+                            FileName: inputFile,
+                            PageNumber: i + j,
+                            X: width * j,
+                            Y: pageHeight - (height * i),
+                            Width: width,
+                            Height: height,
+                            Rotation: 0);
+                    }
+                }
+
+                // Close the new file to complete PDF creation
+                toolkit.CloseOutputFile();
+            }
+
+            // Process Complete
+            WriteResult("Success!");
+        }
+
+        public static void WriteResult(string result, APToolkitNET.Toolkit toolkit = null)
+        {
+            StringBuilder resultText = new StringBuilder();
+            resultText.AppendLine(result);
+            if (toolkit != null)
+            {
+                resultText.AppendLine($"ErrorCode: {toolkit.ExtendedErrorCode.ToString()}");
+                resultText.AppendLine($"Location: {toolkit.ExtendedErrorLocation}");
+                resultText.AppendLine($"Description: {toolkit.ExtendedErrorDescription}");
+            }
+            Console.WriteLine(resultText.ToString());
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VBS/Toolkit.StitchPDF.vbs
@@ -0,0 +1,65 @@
+' Copyright (c) 2019 ActivePDF, Inc.
+
+Dim FSO, strPath, intResult
+
+' Get current path
+Set FSO = CreateObject("Scripting.FileSystemObject")
+strPath = FSO.GetFile(Wscript.ScriptFullName).ParentFolder & "\"
+Set FSO = Nothing
+
+' Instantiate Object
+Set oTK = CreateObject("APToolkit.Object")
+
+numPages = oTK.NumPages(strPath & "Toolkit.Input.pdf")
+If numPages = 0 Then
+	WriteResult "NumPages", intResult
+End If
+
+' The number of pages from the input PDF to place per row.
+pagesPerRow = Round(Sqr(numPages) + 0.5)
+
+oTK.CloseInputFile
+
+' Create the new PDF file
+intResult = oTK.OpenOutputFile(strPath & "Toolkit.StitchPDF.pdf")
+If intResult = 0 Then
+    ' Using the default PDF width and height of 612/792
+	pageWidth = 612.0
+	pageHeight = 792.0
+
+	' The width and height of each page from the original PDF
+	' added to the output file.
+	width = pageWidth / pagesPerRow
+	height = pageHeight / pagesPerRow
+
+	' The rows of images from the original PDF added to the new
+	' document.
+	numRows = Round((pageHeight / height) + 0.5)
+		
+	For i = 1 To numRows
+		For j = 0 To pagesPerRow
+			' Add the page from the original PDF to the output.		
+			oTK.StitchPDF strPath & "Toolkit.Input.pdf", i + j, width * j, pageHeight - (height * i), width, height, 0
+		Next
+	Next
+Else
+    WriteResult "OpenOutputFile", intResult
+End If
+
+' Close the new file to complete PDF creation
+oTK.CloseOutputFile 
+
+' Release Object
+Set oTK = Nothing
+
+MsgBox "Success!"
+
+' Error Handling
+Sub WriteResult(method, outputCode)
+  errorString = "Error in " & method & ": " & outputCode & vbCrlf
+  errorString = errorString & "Extended Error Code: " & oTK.ExtendedErrorCode & vbCrlf
+  errorString = errorString & "Extended Error Location: " & oTK.ExtendedErrorLocation & vbCrlf
+  errorString = errorString & "Extended Error Description: " & oTK.ExtendedErrorDescription & vbCrlf  
+  Wscript.Echo(errorString)
+  WScript.Quit
+End Sub
