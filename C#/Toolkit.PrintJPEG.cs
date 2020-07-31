3e4ca89a46ece04c577a263c3352d1e416177028
Bruce Tompkinson <bruce.tompkinson@activepdf.com>
2020-07-31 08:06:42 -0700

    Create Toolkit.PrintJPEG.cs
    New PrintJPEG example.

---
+++ C#/Toolkit.PrintJPEG.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Text;
+
+namespace ToolkitExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strPath = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            // Starting with Toolkit version 10 native DLLs are no longer
+            // copied to the system folder. The Toolkit constructor must
+            // be called with the path to the native DLLs or place them
+            // in your applications working directory. This example
+            // assumes they are located in the default installation folder.
+            // (Use x86 in the path for 32b applications)
+            string toolkitPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)}\ActivePDF\Toolkit\bin\x64";
+
+            using (APToolkitNET.Toolkit toolkit = new APToolkitNET.Toolkit(toolkitPath))
+            {
+                // Here you can place any code that will alter the output file
+                // Such as adding security, setting page dimensions, etc.
+
+                // Create the new PDF file
+                int result = toolkit.OpenOutputFile($"{strPath}Toolkit.PrintJPEG.pdf");
+                if (result == 0)
+                {
+                    // Open the template PDF
+                    result = toolkit.OpenInputFile($"{strPath}Toolkit.Input.pdf");
+                    if (result == 0)
+                    {
+                        // Print the JPEG to the PDF
+                        toolkit.PrintJPEG($"{strPath}Toolkit.Input.jpg", 05.0f, 350.0f, 0.0f, 0.0f, true, -1);
+
+                        // Here you can call any Toolkit functions that will manipulate
+                        // the input file such as text and image stamping, form filling, etc.
+
+                        // Copy the template (with any changes) to the new file
+                        // Start page and end page, 0 = all pages
+                        result = toolkit.CopyForm(0, 0);
+                        if (result != 1)
+                        {
+                            WriteResult($"Error copying file: {result.ToString()}", toolkit);
+                            return;
+                        }
+
+                        // Close the new file to complete PDF creation
+                        toolkit.CloseOutputFile();
+
+                        WriteResult("Success!");
+                    }
+                    else
+                    {
+                        WriteResult($"Error opening input file: {result.ToString()}", toolkit);
+                        return;
+                    }
+                }
+                else
+                {
+                    WriteResult($"Error opening output file: {result.ToString()}", toolkit);
+                    return;
+                }
+            }
+        }
+
+        public static void WriteResult(string result, APToolkitNET.Toolkit toolkit = null)
+        {
+            StringBuilder resultText = new StringBuilder();
+            resultText.AppendLine(result);
+            if (toolkit != null)
+            {
+                resultText.AppendLine($"ErrorCode: {toolkit.ExtendedErrorCode.ToString()}");
+                resultText.AppendLine($"Location: {toolkit.ExtendedErrorLocation}");
+                resultText.AppendLine($"Description: {toolkit.ExtendedErrorDescription}");
+            }
+            Console.WriteLine(resultText.ToString());
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
