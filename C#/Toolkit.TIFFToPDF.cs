ddc9f1d8ba979f1ecc25c31422b5856b499e2a47
Bruce Tompkinson <bruce.tompkinson@activepdf.com>
2021-01-06 10:56:36 -0800

    Added TIFFToPDF Examples

---
+++ C#/Toolkit.TIFFToPDF.cs
@@ -0,0 +1,56 @@
+
+using System;
+using System.Text;
+
+namespace ToolkitExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strPath = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            // Starting with Toolkit version 10 native DLLs are no longer
+            // copied to the system folder. The Toolkit constructor must
+            // be called with the path to the native DLLs or place them
+            // in your applications working directory. This example
+            // assumes they are located in the default installation folder.
+            // (Use x86 in the path for 32b applications)
+            string toolkitPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)}\ActivePDF\Toolkit\bin\x64";
+
+            // Instantiate Object
+            using (APToolkitNET.Toolkit toolkit = new APToolkitNET.Toolkit(CoreLibPath: toolkitPath))
+            {
+                // Any supported image file can be converted to PDF with ImageToPDF
+                int result = toolkit.TIFFToPDF(
+                    ImageFileName: $"{strPath}Toolkit.Input.tif",
+                    PDFFileName: $"{strPath}Toolkit.TIFFToPDF.pdf");
+                if (result != 1)
+                {
+                    WriteResult($"Error converting image to PDF: {result.ToString()}", toolkit);
+                }
+
+                // Close the new file to complete PDF creation
+                toolkit.CloseOutputFile();
+            }
+
+            // Process Complete
+            WriteResult("Success!");
+        }
+
+        public static void WriteResult(string result, APToolkitNET.Toolkit toolkit = null)
+        {
+            StringBuilder resultText = new StringBuilder();
+            resultText.AppendLine(result);
+            if (toolkit != null)
+            {
+                resultText.AppendLine($"ErrorCode: {toolkit.ExtendedErrorCode.ToString()}");
+                resultText.AppendLine($"Location: {toolkit.ExtendedErrorLocation}");
+                resultText.AppendLine($"Description: {toolkit.ExtendedErrorDescription}");
+            }
+            Console.WriteLine(resultText.ToString());
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VBS/Toolkit.TIFFToPDF.vbs
@@ -0,0 +1,31 @@
+' Copyright (c) 2019 ActivePDF, Inc.
+Dim FSO, strPath, intResult
+
+' Get current path
+Set FSO = CreateObject("Scripting.FileSystemObject")
+strPath = FSO.GetFile(Wscript.ScriptFullName).ParentFolder & "\"
+Set FSO = Nothing
+
+' Instantiate Object
+Set oTK = CreateObject("APToolkit.Object")
+
+' Any supported image file can be converted to PDF with ImageToPDF
+intResult = oTK.TIFFToPDF(strPath & "Toolkit.Input.tif", strPath & "Toolkit.TIFFToPDF.pdf")
+If intResult <> 1 Then
+  WriteResult "ImageToPDF", intResult
+End If
+
+' Release Object
+Set oTK = Nothing
+
+' Process Complete
+Wscript.Echo("Success!")
+
+Sub WriteResult(method, outputCode)
+  errorString = "Error in " & method & ": " & outputCode & vbCrlf
+  errorString = errorString & "Extended Error Code: " & oTK.ExtendedErrorCode & vbCrlf
+  errorString = errorString & "Extended Error Location: " & oTK.ExtendedErrorLocation & vbCrlf
+  errorString = errorString & "Extended Error Description: " & oTK.ExtendedErrorDescription & vbCrlf  
+  Wscript.Echo(errorString)
+  WScript.Quit
+End Sub
