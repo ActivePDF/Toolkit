4cd347899dd2ee26c9695b804c6b8cd11cf2c4a4
Bruce Tompkinson <bruce.tompkinson@activepdf.com>
2021-02-23 09:15:00 -0800

    New AddField Example

---
+++ C#/Toolkit.AddField.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+using System.Text;
+
+namespace ToolkitExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strPath = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            // Starting with Toolkit version 10 native DLLs are no longer
+            // copied to the system folder. The Toolkit constructor must
+            // be called with the path to the native DLLs or place them
+            // in your applications working directory. This example
+            // assumes they are located in the default installation folder.
+            // (Use x86 in the path for 32b applications)
+            string toolkitPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)}\ActivePDF\Toolkit\bin\x64";
+
+            // Instantiate Object
+            using (APToolkitNET.Toolkit toolkit = new APToolkitNET.Toolkit(CoreLibPath: toolkitPath))
+            {
+
+                // Here you can place any code that will alter the output file
+                // Such as adding security, setting page dimensions, etc.
+
+                // Create the new PDF file
+                int result = toolkit.OpenOutputFile(FileName: $"{strPath}Toolkit.AddField.pdf");
+                if (result == 0)
+                {
+                    // Open the template PDF
+                    result = toolkit.OpenInputFile(InputFileName: $"{strPath}Toolkit.Input.pdf");
+                    if (result == 0)
+                    {
+                        // Here you can call any Toolkit functions that will manipulate
+                        // the input file such as text and image stamping, form filling, etc.
+
+                        // Add a new button form to the first page.
+                        APToolkitNET.FieldInfo fieldInfo = toolkit.AddField(1, 3, "TEST_BUTTON", 200, 600, 85, 25, "Helvetica", 10);
+
+                        // Set the field properties with the AddField FieldInfo result object.
+                        fieldInfo.ButtonTextNormal = "TEST BUTTON";
+                        fieldInfo.
+
+                        // Copy the template (with any changes) to the new file
+                        // Start page and end page, 0 = all pages
+                        result = toolkit.CopyForm(FirstPage: 0, LastPage: 0);
+                        if (result != 1)
+                        {
+                            WriteResult($"Error copying file: {result.ToString()}", toolkit);
+                            return;
+                        }
+
+                        // Close the new file to complete PDF creation
+                        toolkit.CloseOutputFile();
+                    }
+                    else
+                    {
+                        WriteResult($"Error opening input file: {result.ToString()}", toolkit);
+                        return;
+                    }
+                }
+                else
+                {
+                    WriteResult($"Error opening output file: {result.ToString()}", toolkit);
+                    return;
+                }
+            }
+
+            // Process Complete
+            WriteResult("Success!");
+        }
+
+        public static void WriteResult(string result, APToolkitNET.Toolkit toolkit = null)
+        {
+            StringBuilder resultText = new StringBuilder();
+            resultText.AppendLine(result);
+            if (toolkit != null)
+            {
+                resultText.AppendLine($"ErrorCode: {toolkit.ExtendedErrorCode.ToString()}");
+                resultText.AppendLine($"Location: {toolkit.ExtendedErrorLocation}");
+                resultText.AppendLine($"Description: {toolkit.ExtendedErrorDescription}");
+            }
+            Console.WriteLine(resultText.ToString());
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
